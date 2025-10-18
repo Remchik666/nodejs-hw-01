ea669535e62f8e2a603c22757b37df3734e80b43
Remchik <art6000.7m@gmail.com>
2025-10-18 17:49:30 +0300

    commit 1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+    try {
+        const data = await readContacts();
+        for (let i = 0; i < number; i++) {
+            data.push(createFakeContact());
+        }
+        await writeContacts(data);
+    } catch {
+        console.log('ERROR GENERATE');
+    }
+};
 
 generateContacts(5);
